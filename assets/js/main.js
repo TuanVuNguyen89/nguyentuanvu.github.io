66df17e5e9bdf9ebac6f86a28932f50b2696160b
TuanVuNguyen89 <100076236+TuanVuNguyen89@users.noreply.github.com>
2022-05-20 21:20:44 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+// $(document).keydown(function(event) {
+//     if (event.keyCode == 123) { // Prevent F12
+//         return false;
+//     } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
+//         return false;
+//     }
+// });
+// $(document).on("contextmenu", function(e) {
+//     e.preventDefault();
+// });
+// setInterval(function() {
+//     debugger;
+// }, 50);
+
+// const btn = document.querySelector(".btn-mode");
+
+// btn.addEventListener("click", function() {
+
+//     document.getElementsByClassName('container')[0].classList.toggle("dark");
+//     for (let i = 0; i <= 1; i++) {
+//         document.getElementsByClassName('heading')[i].classList.toggle("dark");
+//         document.getElementsByClassName('bd')[i].classList.toggle("dark");
+
+
+//     }
+
+// });
